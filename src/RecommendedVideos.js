1adcaf618b5afb06f23fb011c0cac60bec7b8465
Suraj Vatsya <surajkumar742494@gmail.com>
2021-11-01 20:46:46 +0530

    videocard added

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import './RecommendedVideos.css'
+import VideoCard from './VideoCard.js'
+
 function RecommendedVideos() {
   return (
     <div className="recommendedVideos">
       <h2>Recommended</h2>
 
+      <div className="recommendedVideos__videos">
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+      </div>
 
-      
     </div>
   )
 }
---
+++ src/VideoCard.js
@@ -0,0 +1,24 @@
+//rfce
+
+import React from 'react'
+import Avatar from '@mui/material/Avatar';
+function VideoCard({ image, title, channel, timestamp, channelImage, views }) {
+  return (
+    <div className="videoCard">
+      <img src={image} alt="" />
+      <div className="videoCard__info">
+        <Avatar className="videoCard__avatar" alt={channel} src={channelImage} />
+        <div className="video__text">
+          <h4>{title}</h4>
+          <p>{channel}</p>
+          <p>
+            {views} . {timestamp}
+          </p>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default VideoCard
